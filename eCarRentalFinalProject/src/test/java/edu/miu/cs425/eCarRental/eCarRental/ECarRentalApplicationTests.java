f4ea13be6fc1bd0d7afade7b2466686b929016ff
Essey <jesseat93@yahoo.com>
2020-06-08 23:49:42 -0500

    first- project commited

---
+++ eCarRentalFinalProject/src/test/java/edu/miu/cs425/eCarRental/eCarRental/ECarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.eCarRental.eCarRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECarRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
