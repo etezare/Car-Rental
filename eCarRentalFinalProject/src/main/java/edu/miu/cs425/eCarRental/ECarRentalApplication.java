89e010eafc61ee0a3a7c33c4e4481cad4db7ac09
Essey <jesseat93@yahoo.com>
2020-06-09 17:25:38 -0500

    updated

---
+++ eCarRentalFinalProject/src/main/java/edu/miu/cs425/eCarRental/ECarRentalApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.eCarRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECarRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECarRentalApplication.class, args);
+	}
+
+}
