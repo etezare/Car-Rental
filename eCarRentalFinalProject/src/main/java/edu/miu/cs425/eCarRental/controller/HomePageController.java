a5ba4a0ecff9020bc47acfc4803f9c3efb8b98ce
Essey <jesseat93@yahoo.com>
2020-06-09 23:50:29 -0500

    updated

--- eCarRentalFinalProject/src/main/java/edu/miu/cs425/eCarRental/controller/HomePageController.java
+++ eCarRentalFinalProject/src/main/java/edu/miu/cs425/eCarRental/controller/HomePageController.java
@@ -1,4 +1,19 @@
 package edu.miu.cs425.eCarRental.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomePageController {
+
+    @GetMapping(value = {"/","/home","/company/home"})
+    public String homePage() {
+        return "public/home/index";
+    }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "public/login/login";
+    }
+
 }
