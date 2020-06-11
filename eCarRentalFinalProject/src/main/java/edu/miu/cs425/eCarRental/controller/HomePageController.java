002d820b32e26ec06ed7e92d3cfe5bb6c23c61ca
Essey <jesseat93@yahoo.com>
2020-06-11 17:42:33 -0500

    updated

--- eCarRentalFinalProject/src/main/java/edu/miu/cs425/eCarRental/controller/HomePageController.java
+++ eCarRentalFinalProject/src/main/java/edu/miu/cs425/eCarRental/controller/HomePageController.java
@@ -5,15 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomePageController {
+    @GetMapping(value = {"/","/ecarrental","/public/home"})
+    public String home0() {
+        return "redirect:/ecarrental/public/home";
+    }
 
-    @GetMapping(value = {"/","/home","/ecarrental/home"})
-    public String homePage() {
+    @GetMapping(value = {"/public/home","/ecarrental/public/home"})
+    public String home1() {
         return "public/home/index";
     }
 
-    @GetMapping(value = "/login")
-    public String login() {
-        return "public/login/login";
+    @GetMapping(value = {"/public/about","/ecarrental/public/about"})
+    public String about() {
+        return "public/home/about";
     }
 
+//    @GetMapping(value = {"/public/virtualtour","/ecarrental/public/virtualtour"})
+//    public String virtualtour() {
+//        return "public/virtualtour";
+//    }
+//
+//    @GetMapping(value = {"/secured/home","/ecarrental/secured/home"})
+//    public String home2() {
+//        return "secured/index";
+//    }
 }
