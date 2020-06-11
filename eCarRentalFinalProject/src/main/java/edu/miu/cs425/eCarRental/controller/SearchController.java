47b9daefa7c0b1ed866a149291cdd209f333f0c6
Essey <jesseat93@yahoo.com>
2020-06-11 00:05:41 -0500

    updated

--- eCarRentalFinalProject/src/main/java/edu/miu/cs425/eCarRental/controller/SearchController.java
+++ eCarRentalFinalProject/src/main/java/edu/miu/cs425/eCarRental/controller/SearchController.java
@@ -34,7 +34,7 @@ public class SearchController {
     @GetMapping(value = "/company/search")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     public ModelAndView searchVehicles(@RequestParam("start") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate start,
-                                       @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate end) {
+                                       @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         ModelAndView modelAndView = new ModelAndView();
         List<Category> availableCategories = searchService.findAvailableCategories(start, end);
         modelAndView.addObject("availableCategories", availableCategories);
@@ -42,4 +42,5 @@ public class SearchController {
         periodRequested.setStart(start);
         periodRequested.setEnd(end);
         return modelAndView;
+    }
 }
