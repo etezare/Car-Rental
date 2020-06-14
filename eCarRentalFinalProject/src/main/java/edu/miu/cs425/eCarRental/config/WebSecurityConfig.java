e79bf2d93e37d7ed91716bb9fd1b78ac70e71a2b
Biniam Arefaine <binireyes@gmail.com>
2020-06-13 22:05:35 -0500

    updated

--- eCarRentalFinalProject/src/main/java/edu/miu/cs425/eCarRental/config/WebSecurityConfig.java
+++ eCarRentalFinalProject/src/main/java/edu/miu/cs425/eCarRental/config/WebSecurityConfig.java
@@ -1,11 +1,8 @@
 package edu.miu.cs425.eCarRental.config;
-
-import edu.miu.cs425.eCarRental.repository.IUserRepository;
 import edu.miu.cs425.eCarRental.service.serviceimplementation.CarRentalUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableJpaRepositories(basePackageClasses = IUserRepository.class)
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
@@ -43,19 +39,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-
                 .csrf().disable()
                 .headers()
                 .frameOptions().sameOrigin()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/resources/static/**", "/images/**", "/css/**", "/ecarrental/public/**","**/public/**").permitAll()
-                .antMatchers("/", "/ecarrental","/ecarrental/**").permitAll()
+                .antMatchers("/resources/static/**", "/images/**", "/css/**", "/ecarrental/public/**").permitAll()
+//                .antMatchers("/", "/ecarrental/public/search/results").permitAll()
                 .antMatchers("/ecarrental/secured/admin/**", "/resources/secured/admin/**", "/ecarrental/admin/**", "/admin/**").hasRole("ADMIN")
-                .antMatchers("/ecarrental/secured/staff/**", "/resources/secured/staff/**", "/staff/**").hasRole("STAFF")
-                .antMatchers("/ecarrental/secured/customer/**", "/resources/secured/customer/**", "/customer/**").hasRole("CUSTOMER")
-                .antMatchers("/ecarrental/public/search/result/**").hasRole("CUSTOMER")
-                .anyRequest().authenticated()
+                .antMatchers("/ecarrental/secured/staff/**", "/resources/secured/staff/**","/staff/**").hasRole("STAFF")
+                .antMatchers("/ecarrental/secured/customer/**", "/resources/secured/customer/**","/ecarrental/customer/**").hasRole("CUSTOMER")
+//                .antMatchers("/ecarrental/public/search/result/**").hasRole("CUSTOMER")
+//                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/ecarrental/public/home/login")
